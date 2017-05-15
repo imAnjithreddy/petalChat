aa86f22b9b1cca0cfb4c8881ba9a25f9d03b6b21
Anjith Reddy <imanjithreddy@gmail.com>
2017-05-15 07:16:45 +0000

    new changes

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 module.exports = {
   db: {
     
-    //test: "mongodb://petaltest:petaltest@ds127300.mlab.com:27300/petalchat",
-    //development: "mongodb://petalchatdev:petalchatdev@ds131320.mlab.com:31320/petalchatdev",
-    //production: "mongodb://petalchatdev:petalchatdev@ds131320.mlab.com:31320/petalchatdev"
+   
   },
   secret:{
     google: 'Z8Kv94_4l6-rgCRBlWszy1On',
