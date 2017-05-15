4e3504095a0e8dd27937552af6f73569cf650699
Anjith Reddy <imanjithreddy@gmail.com>
2017-05-15 07:15:39 +0000

    availabe changes

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   db: {
     
-    test: "mongodb://petaltest:petaltest@ds127300.mlab.com:27300/petalchat",
-    development: "mongodb://petalchatdev:petalchatdev@ds131320.mlab.com:31320/petalchatdev",
-    production: "mongodb://petalchatdev:petalchatdev@ds131320.mlab.com:31320/petalchatdev"
+    //test: "mongodb://petaltest:petaltest@ds127300.mlab.com:27300/petalchat",
+    //development: "mongodb://petalchatdev:petalchatdev@ds131320.mlab.com:31320/petalchatdev",
+    //production: "mongodb://petalchatdev:petalchatdev@ds131320.mlab.com:31320/petalchatdev"
   },
   secret:{
     google: 'Z8Kv94_4l6-rgCRBlWszy1On',
